aa9cef8ad8f965c2b4ab17558fc45c885d24ee29
jeremey van der Hansz <jeremeymain@gmail.com>
2020-11-20 01:35:09 +0100

    commoit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+import store from './store'
 
-createApp(App).mount('#app')
+Vue(App).mount('#app')
+
+new Vue({
+    VueRouter,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount("#app");
